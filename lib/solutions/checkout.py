9a08f77618cf6d98700350ea9ee4e8da116d2d3d
sbx_user1060 <sbx_user1060@ip-10-11-38-23.eu-west-2.compute.internal>
2018-06-02 21:04:35 +0000

    Sat Jun 02 21:04:35 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -3,4 +3,9 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+	valids = ['a','b','c']
+	skus = skus.lower()
+	
+    for i in skus:
+    	if i not in valids:
+    		return -1
