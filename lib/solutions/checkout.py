5a89b8b368bc6bd69754dc0195ceb92a690f448f
sbx_user1060 <sbx_user1060@ip-10-11-38-23.eu-west-2.compute.internal>
2018-06-02 21:16:34 +0000

    Sat Jun 02 21:16:34 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -5,7 +5,36 @@
 def checkout(skus):
 	valids = ['a','b','c']
 	skus = skus.lower()
-	
-    for i in skus:
-    	if i not in valids:
+
+	count = {}
+
+    for char in skus:
+    	if char not in valids:
     		return -1
+    	if char in count:
+    		count[char] += 1
+    	else:
+    		count[char] = 1
+
+    if  count['a'] >= 3:
+    	i = count['a']
+    	aCounter = 0
+    	while i >= 3:
+    		i = count['a'] % 3  
+    		aCounter += 1
+
+    	aAmount = aCounter*130 + i*50
+
+    if count['b'] >= 2:
+    	j = count['b']
+    	bCounter = 0
+    	while j >= 2:
+    		j = count['b'] % 2  
+    		bCounter += 1
+
+    	bAmount = bCounter*45 + 30
+
+
+
+
+
