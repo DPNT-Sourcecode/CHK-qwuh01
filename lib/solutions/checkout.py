ff66b3ad69458eba6c0d8a67985d8528f05ae5bd
sbx_user1060 <sbx_user1060@ip-10-12-190-242.eu-west-2.compute.internal>
2018-06-03 17:07:23 +0000

    Sun Jun 03 17:07:23 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -6,34 +6,36 @@
 
 def checkout(skus):
 
-  if type(skus) != type(""):
+  if not isinstance(skus, unicode):
     return -1
 
   aAmount, bAmount, cAmount, dAmount = 0, 0, 0, 0
 
   count = {'A': skus.count('A'), 'B': skus.count('B'), 'C': skus.count('C'), 'D': skus.count('D')}
 
-  if "A" in count.keys() and count["A"] >= 3:
+  items = count.keys()
+
+  if "A" in items and count["A"] >= 3:
       i = count["A"] % 3  
       aCounter = count["A"] / 3 
       aAmount = aCounter*130 + i*50
-  elif "A" in count.keys():
+  elif "A" in items:
   	aAmount = count["A"] * 50
 
 
-  if "B" in count.keys() and count["B"] >= 2:
+  if "B" in items and count["B"] >= 2:
       j = count["B"] % 2  
       bCounter = count["B"] / 2
       bAmount = bCounter*45 + j*30
-  elif "B" in count.keys():
+  elif "B" in items:
   	bAmount = count["B"] * 30
 
 
-  if "C" in count.keys():
+  if "C" in items:
     cAmount = count["C"] * 20
 
 
-  if "D" in count.keys():  
+  if "D" in items:  
     dAmount = count["D"] * 15
 
   return aAmount + bAmount + cAmount + dAmount
