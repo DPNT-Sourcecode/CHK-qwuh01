6f407f5b5e3d01aa8a5f6d92306d870c889ed174
sbx_user1059 <sbx_user1059@ip-10-11-24-121.eu-west-2.compute.internal>
2018-06-03 09:06:16 +0000

    Sun Jun 03 09:06:16 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -21,28 +21,32 @@ def checkout(skus):
     	count[char] = 1
 
 
-    if count['A'] >= 3:
-    	i = count['A']
-    	aCounter = 0
-    	while i >= 3:
-    		i = count['A'] % 3  
-    		aCounter += 1
+  if count['A'] >= 3:
+    i = count['A']
+    aCounter = 0
+    while i >= 3:
+      i = count['A'] % 3  
+      aCounter += 1
 
-    	aAmount = aCounter*130 + i*50
+      aAmount = aCounter*130 + i*50
+  else:
+  	aAmount = count['A'] * 50
 
-    if count['B'] >= 2:
-    	j = count['B']
-    	bCounter = 0
-    	while j >= 2:
-    		j = count['B'] % 2  
-    		bCounter += 1
+  if count['B'] >= 2:
+    j = count['B']
+    bCounter = 0
+    while j >= 2:
+      j = count['B'] % 2  
+      bCounter += 1
 
-    	bAmount = bCounter*45 + 30
+      bAmount = bCounter*45 + j*30
+  else:
+  	bAmount = count['B'] * 30
 
-    cAmount = count['C'] * 20
-    dAmount = count['D'] * 15
+  cAmount = count['C'] * 20
+  dAmount = count['D'] * 15
 
-    return aAmount + bAmount + cAmount + dAmount
+  return aAmount + bAmount + cAmount + dAmount
 
 print checkout('ABCD')
 
